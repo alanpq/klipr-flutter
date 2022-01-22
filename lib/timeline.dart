778a7d59091fb6dd8a8702765c947c62f4074ed8
Alan Panayotov <alanpanayotov@gmail.com>
2022-01-22 20:39:57 +0000

    remove imports

--- lib/timeline.dart
+++ lib/timeline.dart
@@ -1,9 +1,4 @@
-import 'dart:ui';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-import 'dart:math';
 
 class Timeline extends StatefulWidget {
   final OnScrubFunc? onScrub;
